477997a0be0352cd8e02b3a4567001d37e5b79cd
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2019-12-17 09:19:34 +0000

    add compute_asl_loglike.R

---
+++ R/compute_asl_loglik.R
@@ -0,0 +1,47 @@
+#' Compute log likelihood
+#'
+#' \code{compute_lcm_loglik} compute log likelihood of data under the latent cause model.
+#'
+#' @param alpha concentration parameter
+#' @param data single-subject data
+#' @param n_cs number of CS
+#' @param opts options structure
+#'
+#' @return lik: log-likelihood
+#' @return latents: structure containing latent variables:
+#'
+#'               b: beta coefficient mapping model CR to  measured CR
+#'
+#'               sd: maximum likelihood standard deviation
+#'
+#'               CR: predicted CR
+#' @export
+#' @examples
+#' # results <- compute_lcm_loglik(alpha,data,n_cs,opts)
+
+compute_lcm_loglik <- function(alpha, data, n_cs, opts) {
+  # set concentration parameter
+  opts <- list()
+  opts$c_alpha <- alpha
+  # un particle filter
+  max_index_cs <- 2 + n_cs
+  results <- associative_structure_learning(cbind(data$US, data[, 3:max_index_cs]), opts)
+  # use linear regression to fit model output to CR
+  N <- length(results$V)
+  X <- results$V
+  # maximum likelihood regression coefficients
+  b <- (t(X) %*% data$CR)/(t(X) %*% X)
+  # predicted CR
+  CRpred <- X %*% b
+  # maximum likelihood standard deviation
+  sd = sqrt(mean((data$CR - CRpred)^2))
+  # log-likelihood
+  lik = sum(log(dnorm(data$CR - CRpred, mean = 0, sd = sd)))
+  # return latent variables
+  latents <- list()
+  latents$results = results
+  latents$b = b
+  latents$sd = sd
+  latents$CR = CRpred
+  return(list(lik = lik, latents = latents))
+}
