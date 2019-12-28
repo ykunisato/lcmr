4797b0931fbd83aebf57e52b5a26b6a9bdf6102c
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2019-12-28 15:28:57 +0000

    major update(add infer_lcm_rw.R and add estimation by optim)

--- R/fit_lcm.R
+++ R/fit_lcm.R
@@ -2,13 +2,14 @@
 #'
 #' \code{fit_lcm} fit latent cause model to conditioning data
 #'
-#' @importFrom pracma linspace
 #' @importFrom magrittr %>%
 #' @importFrom dplyr group_by
 #' @importFrom dplyr mutate
 #' @importFrom tidyr nest
 #' @importFrom purrr map
 #' @importFrom tidyr unnest
+#' @importFrom furrr future_map
+#' @importFrom future plan
 #'
 #' @param data long format data containing the following variables
 #'        (Order and name is exactly the same as following):
@@ -19,12 +20,12 @@
 #'
 #'        US  Unconditioned Stimulus
 #'
-#'        CS  Conditioned Stimului. If using multiple CS, set variables name as CS1,CS2,CS3...
-#' @param n_cs number of CS
-#' @param opts (optional) structure defining LCM options (see LCM_opts)
-#' @param set_alpha (optional) option of alpha values to evaluate.
-#'        You can change max value and numbers of alpha as followings(default: max value = 10, number = 50).
-#'        list(num_alpha = 100, max_alpha = 5)
+#'        CS  Conditioned Stimului or Context. If using multiple CS, set variables name as CS1,CS2,CS3...
+#' @param model 1 = latent cause model, 2 = latent cause modulated RW model
+#' @param opts (optional) structure defining ASL options
+#' @param parameter_range (optional)  range of parameter(a_L, a_U, e_L, e_U)
+#' @param parallel (optional)  0 = single, 1 = parallel
+#' @param estimation_method (optional)  0 = optim or optimize(lcm), 1 = post mean(only latent cause model)
 #'
 #' @return return the fit, parameters and plc_vcr.
 #' fit is fitting results. parameters is parameters estmated including post_mean_alpha(posterior mean alpha) and
@@ -34,47 +35,214 @@
 #' @export
 #' @examples
 #'
-#' # results <- fit_lcm(data,n_cs,opts,list(num_alpha = 100, max_alpha = 10))
-fit_lcm <- function(data, n_cs, opts, set_alpha) {
+#' # results <- fit_lcm(data, model, opts, parameter_range, parallel, estimation_method)
+fit_lcm <- function(data, model, opts, parameter_range, parallel, estimation_method){
     # check argument
+    if (missing(data)) {
+        stop("Please set the data")
+    }
+    if (missing(model)) {
+        stop("Please set the model")
+    }
     if (missing(opts)) {
         opts <- list()
     }
-    if (missing(n_cs)) {
-        warning("Please set the n_cs(number of CS)")
+    if (missing(parallel)) {
+        parallel <- 0
     }
-    if (missing(set_alpha)) {
-        # set default alpha to evaluate
-        alpha <- linspace(0, 10, 50)
-    }else{
-        # set user defined alpha to evaluate
-        alpha <- linspace(0, set_alpha$max_alpha, set_alpha$num_alpha)
+    if (missing(estimation_method)) {
+        estimation_method <- 0
+    }
+    # set parameters range
+    def_parameter_range <- list()
+    def_parameter_range$a_L <- 0.0000000000000000000000000000001
+    def_parameter_range$a_U <- 10
+    def_parameter_range$e_L <- 0.0000000000000000000000000000001
+    def_parameter_range$e_U <- 1
+    if (missing(parameter_range)) {
+        parameter_range = def_parameter_range
+    } else {
+        F <- names(def_parameter_range)
+        for (i in 1:length(F)) {
+            if (eval(parse(text = paste0("length(parameter_range$", F[i], ")==0")))) {
+                eval(parse(text = paste0("parameter_range$", F[i], "=def_parameter_range$", F[i])))
+            }
+        }
     }
     # fitting
-    fit <- data %>%
-        group_by(ID) %>%
-        nest() %>%
-        mutate(fit = map(data, ~estimate_lcm_a(data = ., n_cs, opts, alpha))) %>%
-        unnest(cols = fit)
+    if(model == 1){
+        if(estimation_method==0){
+            if(parallel==0){
+                fit  <- data %>%
+                    group_by(ID) %>%
+                    nest() %>%
+                    mutate(fit = map(data, ~optimize(compute_negative_loglike,
+                                                     interval = c(parameter_range$a_L, parameter_range$a_U),
+                                                     data = ., model = model, opts = opts))) %>%
+                    unnest_wider(fit) %>%
+                    rename(alpha=minimum,nll=objective)
+            }else if(parallel==1){
+                # set parallel computing
+                plan("multisession")
+                # fitting
+                fit <- data %>%
+                    group_by(ID) %>%
+                    nest() %>%
+                    mutate(fit = future_map(data, ~optimize(compute_negative_loglike,
+                                                            interval = c(parameter_range$a_L, parameter_range$a_U),
+                                                            data = ., model = model, opts = opts))) %>%
+                    unnest_wider(fit) %>%
+                    rename(alpha=minimum,nll=objective)
+            }
+        }else if(estimation_method==1){
+            alpha <- linspace(0, 10, 50)
+            if(parallel==0){
+                fit <- data %>%
+                    group_by(ID) %>%
+                    nest() %>%
+                    mutate(fit = map(data, ~estimate_by_post_mean(data = ., model, opts, alpha))) %>%
+                    unnest_wider(fit) %>%
+                    rename(alpha = post_mean_alpha)
+
+            }else if(parallel==1){
+                # set parallel computing
+                plan("multisession")
+                # fitting
+                fit <- data %>%
+                    group_by(ID) %>%
+                    nest() %>%
+                    mutate(fit = future_map(data, ~estimate_by_post_mean(data = ., model, opts, alpha))) %>%
+                    unnest_wider(fit) %>%
+                    rename(alpha = post_mean_alpha)
+            }
+        }
+    }else if(model == 2){
+        if(parallel==0){
+            fit  <- data %>%
+                group_by(ID) %>%
+                nest() %>%
+                mutate(fit = map(data, ~estimate_by_optim(data = ., model, opts, parameter_range))) %>%
+                unnest_wider(fit)
+        }else if(parallel==1){
+            # set parallel computing
+            plan("multisession")
+            # fitting
+            fit  <- data %>%
+                group_by(ID) %>%
+                nest() %>%
+                mutate(fit = future_map(data, ~estimate_by_optim(data = ., model, opts, parameter_range))) %>%
+                unnest_wider(fit)
+        }
+    }
     # extract matrix of latent cause posterior and V & CR predicted
     b <- NULL
     sd <- NULL
     plc_vcr <- NULL
 
     for (i in 1:length(fit$ID)) {
-        estimate <- compute_lcm_loglik(fit$post_mean_alpha[i], fit$data[[i]], n_cs, opts)
-        b[i] <- estimate$latents$b
-        sd[i] <- estimate$latents$sd
-        plc_vcr <-rbind(plc_vcr, data.frame(ID = rep(fit$ID[i],length(estimate$latents$results$V)),
+        if(is.nan(fit$alpha[i])){
+            b[i] <- NaN
+            sd[i] <- NaN
+        }else{
+            if(model==1){
+                estimate <- compute_loglike(fit$alpha[i], fit$data[[i]], model, opts)
+            }else if(model==2){
+                estimate <- compute_loglike(c(fit$alpha[i],fit$eta[i]), fit$data[[i]], model, opts)
+            }
+
+            b[i] <- estimate$latents$b
+            sd[i] <- estimate$latents$sd
+            plc_vcr <-rbind(plc_vcr, data.frame(ID = rep(fit$ID[i],length(estimate$latents$results$V)),
                              v =estimate$latents$results$V,
                              cr = estimate$latents$CR,
-                             estimate$latents$results$post))
+                             estimate$latents$results$Z))
+        }
     }
-
-    parameters <- data.frame(ID = fit$ID,
-                             post_mean_alpha = fit$post_mean_alpha,
-                             b = b,
-                             sd = sd)
-
+    if(model==1){
+        parameters <- data.frame(ID = fit$ID,
+                                 alpha = fit$alpha,
+                                 b = b,
+                                 sd = sd)
+    }else if(model==2){
+        parameters <- data.frame(ID = fit$ID,
+                                 alpha = fit$alpha,
+                                 eta = fit$eta,
+                                 b = b,
+                                 sd = sd)
+    }
     return(list(fit = fit, parameters = parameters, plc_vcr = plc_vcr))
+}
+
+
+
+
+
+#' Estimate alpha of LCM by post mean
+#'
+#' \code{estimate_by_post_mean} is function to estimate alpha and logBF
+#' @importFrom tidyr nest
+#'
+#' @param alpha vector of alpha
+#' @param data data from fit_asl or fit_asl_parallel
+#' @param model 1 = latent cause model, 2 = latent cause modulated RW model
+#' @param opts (optional) structure defining ASL options
+#'
+#' @return df data frame containing post_mean_alpha(posterior mean alpha),
+#' logBF(log Bayes factor for the alpha>=0 model relative to the alpha=0 model),
+#' loglikelihood and probability each alpha.
+estimate_by_post_mean <- function(data, model, opts, alpha) {
+    lik <- numeric(length(alpha))
+    for (i in 1:length(alpha)) {
+        results <- compute_loglike(param=alpha[i], data, model, opts)
+        lik[i] <- results$lik
+    }
+    L <- log(sum(exp(lik)))
+    P <- exp(lik - L)
+    post_mean_alpha <- alpha %*% P
+    logBF <- L - log(length(alpha)) - lik[1]
+    df <- data.frame(post_mean_alpha,
+                     logBF,
+                     data.frame(alpha,lik,P))%>%
+        nest(prob_alpha=c(alpha,lik,P))
+    return(df)
+}
+
+
+
+
+#' Estimate parameters of LCM by optim
+#'
+#' \code{estimate_by_optim} is function to estimate lcm parameter using optim
+#' @importFrom tidyr nest
+#'
+#' @param data data from fit_asl or fit_asl_parallel
+#' @param model 1 = latent cause model, 2 = latent cause modulated RW model
+#' @param opts (optional) structure defining ASL options
+#' @param parameter_range (optional) range of parameter
+#'
+#' @return estimated parameters, negative log likelihood
+estimate_by_optim <- function(data, model, opts, parameter_range) {
+    smallest_nll <- Inf
+    cat("start estimation using optim... \n")
+    for (i in 1:10) {
+        #compute_negative_loglike(param, data, model, opts)
+        init_param <- c(runif(1,parameter_range$a_L,parameter_range$a_U),
+                        runif(1,parameter_range$e_L,parameter_range$e_U))
+        try({
+        results <- optim(init_param,
+                         compute_negative_loglike,
+                         data = data, model = model, opts = opts,
+                         method = "L-BFGS-B",
+                         lower = c(parameter_range$a_L,parameter_range$e_L),
+                         upper = c(parameter_range$a_U,parameter_range$e_U))
+
+        cat("negative log likelihood: ",results$value)
+        cat("  parameter: ",results$par,"\n")
+        if(results$value < smallest_nll){
+            smallest_nll <- results$value
+            param <- results$par
+        }
+        })
+    }
+    return(list(alpha = param[1], eta = param[2], nll = smallest_nll))
 }
---
+++ R/infer_lcm.R
@@ -0,0 +1,316 @@
+#' Latent cause model using particle filtering or local maximum a posteriori inference
+#'
+#' \code{infer_lcm} conduct Particle filtering or local maximum a posteriori inference
+#' for latent cause model of associative learning
+#'
+#' @importFrom pracma histc
+#'
+#' @param X matrix of stimulus inputs consisting of the number of trial rows and
+#' the number of stimulus features columns. The first feature (column 1) is the US,
+#' and the rest of the features (column 2 through D) are CSs.
+#' @param opts (optional)structure containing various options (see LCM_opts).
+#' Missing fields are set to defaults. If opts.M = 1, then the model computes a local MAP estimate.
+#' @return opts: options used in inference
+#' @return V: US prediction each trial
+#' @return post: matrix of latent cause posterior consisting of the number of trial rows and
+#' the probability of latent cause k being active on trial t, after observing the all the features.
+#' K (the number of latent causes) is determined adaptively by the model.
+#' @export
+#' @examples
+#' # results <- infer_lcm(X,opts)
+
+infer_lcm <- function(X, opts) {
+    # set default options
+    def_opts <- list()
+    def_opts$M <- 100
+    def_opts$a <- 1
+    def_opts$b <- 1
+    def_opts$c_alpha <- 0
+    def_opts$stickiness <- 0
+    def_opts$K <- 10
+    # set parameters
+    results <- list()
+    if (nargs() < 2) {
+        opts = def_opts
+    } else {
+        F <- names(def_opts)
+        for (i in 1:length(F)) {
+            if (eval(parse(text = paste0("length(opts$", F[i], ")==0")))) {
+                eval(parse(text = paste0("opts$", F[i], "=def_opts$", F[i])))
+            }
+        }
+    }
+
+    M <- opts$M
+    a <- opts$a
+    b <- opts$b
+    K <- opts$K
+    results$opts <- opts
+
+    # initialization
+    post <- matrix(0, 1, K)
+    post[1] <- 1
+    # posterior probability of state(M=number of particles, K=number of state)
+    post0 <- matrix(0, M, K)
+    post0[, 1] <- 1
+    T <- nrow(X)
+    D <- ncol(X)
+    # feature-cause co-occurence counts(particle*state*stim) stimuli On
+    N <- array(0, dim = c(M, K, D))
+    # feature-cause co-occurence counts(particle*state*stim) stimuli off
+    B <- array(0, dim = c(M, K, D))
+    # cause counts(particle*state)
+    Nk <- matrix(0, M, K)
+    # cause assignments (trial*state，value of first row is 1)
+    results$post <- cbind(matrix(1, T, 1), matrix(0, T, K - 1))
+    # US predictions(trials)
+    results$V <- matrix(0, T, 1)
+    # number of particles
+    z <- matrix(1, M, 1)
+
+    # loop over trials
+    for (t in 1:T) {
+        # calculate likelihood Set Mkd in stimulus on (particles*state*stim)
+        Mkd <- N
+        # if simlus is not presented, insert Mkd in simulus off
+        Mkd[, , X[t, ] == 0] <- B[, , X[t, ] == 0]
+        # calculate likelihood using supple equation 6
+        numerator_lik <- Mkd + a
+        denominator_lik <- Nk + a + b
+        lik <- N
+        for (d in 1:D) {
+            lik[, , d] <- numerator_lik[, , d]/denominator_lik
+        }
+        # only update posterior if concentration parameter is non-zero
+        if (opts$c_alpha > 0) {
+            # calculate CRP prior
+            prior <- Nk
+            for (m in 1:M) {
+                # add stickiness(if stickiness is higher than 0, number of state leadns 1)
+                prior[m, z[m]] <- prior[m, z[m]] + opts$stickiness
+                # probability of a new latent cause(Insert alpha in non active state)
+                prior[m, which(prior[m, ] == 0)[1]] <- opts$c_alpha
+            }
+            # posterior conditional on CS only element-wise product of prior and likelihood of CS
+            # using supple 2nd term of equation 11
+            num_add_cs <- D - 2  #if multiple CS are used, additional number of CS(use two CS, num_add_cs is 1)
+            if (num_add_cs == 0) {
+                prod_like_cs <- lik[, , 2]
+            } else {
+                prod_like_cs <- lik[, , 2]
+                for (d in 1:num_add_cs) {
+                    prod_like_cs <- prod_like_cs * lik[, , 2 + d]
+                }
+            }
+            post <- prior * drop(prod_like_cs)
+            # divide posterior rowsum of posterior(is mean prob of CS)
+            post0 <- post/rowSums(post)
+
+            # posterior conditional on CS and US element-wise product of posterior of CS and
+            # likelihood of US using supple 1st term of equation 11
+            post <- post * drop(lik[, , 1])
+            # posterior of US is devided by row sum of posteriro of US（probability of US）
+            post <- post/rowSums(post)
+            # marginalize over particles
+            post <- colMeans(post)
+        }
+        # output of results
+        results$post[t, ] = post
+        # posterior predictive mean for US likelihoof of US
+        pUS = drop(N[, , 1] + a)/(Nk + a + b)
+        # product of posteriro of CS and likelihood of US is devided by number of particles
+        results$V[t, 1] = t(as.vector(post0)) %*% as.vector(pUS)/M
+
+        # sample new particles
+        x1 <- X[t, ] == 1
+        x0 <- X[t, ] == 0
+        if (M == 1) {
+            # maximum a posteriori
+            z = which.max(post)
+        } else {
+            # multinomial sample
+            z <- histc(runif(M, 0, 1), c(0, cumsum(post)))$bin
+        }
+        for (m in 1:M) {
+            Nk[m, z[m]] <- Nk[m, z[m]] + 1
+            N[m, z[m], x1] <- N[m, z[m], x1] + 1
+            B[m, z[m], x0] <- B[m, z[m], x0] + 1
+        }
+    }
+    # remove unused state
+    # results$post <- results$post[, colMeans(results$post) != 0]
+    return(list(opts = opts, V = results$V, post = results$post))
+}
+
+
+
+
+
+#' Latent cause modulated Rescorla-Wagner model
+#'
+#' \code{infer_lcm_rw} conduct local maximum a posteriori inference
+#' for associative and structuallearning
+#'
+#' @importFrom pracma histc
+#' @importFrom pracma repmat
+#'
+#' @param X matrix cotaining time(stimulus onset, unit is sec), US, CS.
+#' @param opts (optional)list containing various options.
+#' If you don't want to use  a nonlinear sigmoidal transformation, you set opts$nst = 0.
+#' @return V: vector of conditioned response on each trial
+#' @return Zp: latent cause posterior before observing US(Trial*K)
+#' @return Z: latent cause posterior(Trial*K)
+#' @export
+#' @examples
+#' # results <- infer_lcm_rw(X, opts)
+#'
+infer_lcm_rw <- function(X, opts){
+    # set options
+    def_opts <- list()
+    def_opts$c_alpha <- 0.1
+    def_opts$g <- 1
+    def_opts$psi <- 0
+    def_opts$eta <- 0.2
+    def_opts$maxIter <- 3
+    def_opts$w0 <- 0
+    def_opts$sr <- 0.4
+    def_opts$sx <- 1
+    def_opts$theta <- 0.03
+    def_opts$lambda <- 0.005
+    def_opts$K <- 15
+    def_opts$nst <- 1
+
+    if (nargs() < 2) {
+        opts <- NULL
+    }
+    if (length(opts) == 0) {
+        opts = def_opts
+    } else {
+        F <- names(def_opts)
+        for (i in 1:length(F)) {
+            if (eval(parse(text = paste0("length(opts$", F[i], ")==0")))) {
+                eval(parse(text = paste0("opts$", F[i], "=def_opts$", F[i])))
+            }
+        }
+    }
+
+    # Initialization
+    zp_save <- NULL
+    w_save <- NULL
+    p_save <- NULL
+    w_before_r <- NULL
+    post_before_r <- NULL
+    X <- matrix(as.matrix(X), nrow(X), ncol(X))
+    T <- nrow(X)
+    time <- X[,1]      #Time
+    r <- X[,2]         #us
+    X <- X[,3:ncol(X)] # cues
+    D <- ncol(X)
+    if(length(opts$c_alpha)==1){
+        opts$c_alpha <-opts$c_alpha*matrix(1,T,1)
+    }
+    if(length(opts$eta)==1){
+        opts$eta <-opts$eta*matrix(1,T,1)
+    }
+    psi <- opts$psi*matrix(1,T,1)
+    Z <- matrix(0,T,opts$K)
+    V <- matrix(0,T,1)
+    W <- matrix(0,D,opts$K) + opts$w0
+
+    # construct distance matrix
+    Dist <- matrix(0,T,T)
+    for (i in 1:T) {
+        for (j in 1:T) {
+            Dist[i,j] = abs(time[i]-time[j])
+        }
+    }
+    S <- Dist^(-opts$g)
+
+    ## Run inference
+    for (t in 1:T) {
+        # determine how many EM iterations to perform based on ITI
+        if(t == T){
+            nIter <- 1
+        }else{
+            nIter <- min(opts$maxIter,round(Dist[t,t+1]))
+        }
+
+        ## calculate (unnormalized) posterior, not including reward
+        #cluster counts
+        if(t == 1){
+            N <- matrix(0,1,opts$K)
+        }else if(t == 2){
+            N <- Z[1,]
+        }else{
+            N <- colSums(Z[1:t-1,])
+        }
+
+        if(opts$c_alpha[t]==0){
+            prior <- matrix(0,1,opts$K)
+            #always use cause1
+            prior[1] <- 1
+        }else{
+            # ddCRP prior
+            if(t == 1){
+                prior <- matrix(0,1,opts$K)
+            }else{
+                prior <- S[1:t-1,t]%*%Z[1:t-1,]
+            }
+            # probability of new cluster
+            prior[,which(prior == 0)][1] <- opts$c_alpha[t]
+        }
+        # normalize prior
+        L <- prior/sum(prior)
+        # [D x K] matrix of feature sums
+        if(t == 1){
+            xsum <- matrix(0,D,1)%*%matrix(0,1,opts$K)
+        }else{
+            xsum <- matrix(X[1:t-1,],D,t-1)%*%Z[1:t-1,]
+        }
+        nu <- opts$sx/(N+opts$sx) + opts$sx
+        for (d in 1:D) {
+            xhat <-  xsum[d,]/(N+opts$sx)
+            L <-  L*dnorm(X[t,d],xhat,sqrt(nu))  # likelihood
+        }
+        # reward prediction, before feedback
+        post <- L/sum(L)
+        V[t] <- (X[t,]%*%W)%*%t(post)
+        w_before_r  <- rbind(w_before_r, W)
+        post_before_r  <- rbind(post_before_r, post)
+        if(opts$nst==1){
+            V[t] <- 1-pnorm(opts$theta,V[t],opts$lambda);
+        }
+        # loop over EM iterations
+        for (iter in 1:nIter){
+            V_afterUS <- X[t,]%*%W                               # reward prediction
+            post <- L*dnorm(r[t],V_afterUS,sqrt(opts$sr))   # unnormalized posterior with reward
+            post <- post/sum(post)
+            rpe <- repmat((r[t]-V_afterUS)*post,D,1)           # reward prediction error
+            x <- repmat(matrix(X[t,]),1,opts$K)
+            W <- W + opts$eta[t]*x*rpe            # weight update
+            if (psi[t]>0){
+                W <- W*(1-repmat(post,D,1))*psi[t]
+            }
+            post_save <- data.frame(t = rep(t,nrow(post)),iter = rep(iter,nrow(post)), post)
+            p_save <- rbind(p_save,post_save)
+            W2_save <- data.frame(t = rep(t,nrow(W)),iter = rep(iter,nrow(W)), W)
+            w_save <- rbind(w_save,W2_save)
+        }
+        # save zp
+        zp_save <- rbind(zp_save,post)
+
+        # cluster assignment
+        k <- which.max(post)                 # maximum a posteriori cluster assignment
+        Z[t,k] <- 1
+    }
+    return(list(opts = opts,
+                Dist = Dist,
+                V = V,
+                Z = Z,
+                S = S,
+                Zp = zp_save,
+                W = w_save,
+                P = p_save,
+                w = w_before_r,
+                p = post_before_r))
+}
